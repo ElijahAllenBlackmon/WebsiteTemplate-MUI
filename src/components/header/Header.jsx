b2247bfeaf190f40ee7e0d192b72244ada0ff446
Elijah Allen Blackmon <elijahblackmon1010@gmail.com>
2022-12-09 03:28:25 -0600

    First Commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import styled from './Header.module.css'
+import Button from '@mui/material/Button';
+
+
+const Header = () => {
+  return (
+    <div>
+        <Button variant="contained">Hello World</Button>
+    </div>
+  )
+}
+
+export default Header
